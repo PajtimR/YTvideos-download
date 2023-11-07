5cab8cc882f95cc71cf672d86e01bc3907237ae8
PajtimR <pajta918@gmail.com>
2023-11-08 00:04:46 +0100

    main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+import pytube
+import youtube_downloader
+
+print("Welcome")
+
+print('''Please choose one option:
+(1) Download YouTube Videos Manually
+(2) Download a YouTube Playlist''')
+
+choice = input("Choice: ")
+
+if choice == "1" or choice == "2":
+    quality = input("Please choose a quality (low, medium, high, very high):")
+    if choice == "2":
+        link = input("Enter the link to the playlist: ")
+        print("Downloading playlist...")
+        youtube_downloader.download_playlist(link, quality)
+        print("Download finished!")
+    if choice == "1":
+        links = youtube_downloader.input_links()
+        for link in links:
+            youtube_downloader.download_video(link, quality)
+else:
+    print("Invalid input! Terminating...")
